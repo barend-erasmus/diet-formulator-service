616c804bfc1562343134e8a6ee3c62f005eb650f
developersworkspace <developersworkspace@gmail.com>
2017-12-09 13:00:04 +0200

    update

--- src/importer/scripts/clean-up.sql
+++
@@ -1,2 +0,0 @@
-UPDATE public."diets" SET "name" = REPLACE("name", '�', '');
-UPDATE public."ingredients" SET "name" = REPLACE("name", '�', '');
--- src/importer/scripts/setup.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public."dietGroups" ("description", "name", "createdAt", "updatedAt", "applicationId")
-VALUES
-(null, 'User Defined', NOW(), NOW(), 1);
