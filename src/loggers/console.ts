bd3ccc1cb6d47865a09984cc88700866d22b0be4
developersworkspace@gmail.com <developersworkspace@gmail.com>
2018-02-24 17:54:11 +0200

    update

---
+++ src/loggers/console.ts
@@ -0,0 +1,26 @@
+import { injectable } from 'inversify';
+import { ILogger } from '../interfaces/logger';
+
+@injectable()
+export class ConsoleLogger implements ILogger {
+
+    constructor() {
+
+    }
+
+    public debug(message: string, metaData?: any): void {
+        console.log(message);
+    }
+
+    public error(error: Error, metaData?: any): void {
+        console.error(error);
+    }
+
+    public info(message: string, metaData?: any): void {
+        console.log(message);
+    }
+
+    public warning(message: string, metaData?: any): void {
+        console.log(message);
+    }
+}
